286af8973164532d39e0bb3f2d83dc38d657f359
unknown <dmshastel@gmail.com>
2025-10-27 13:57:21 +0300

    The store is created

--- Shop/src/index.tsx
+++ Shop/src/index.tsx
@@ -1,13 +1,24 @@
 import {createRoot} from "react-dom/client";
 import {App} from "./common/components/app/App";
 import "./global.scss"
+import {store} from "./service/store";
+import { Provider } from 'react-redux'
 
-const root = document.getElementById('root')
+const container = document.getElementById('root')
 
-if(!root){
-    throw new Error("Root element not found")
+if (container) {
+    const root = createRoot(container)
+
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+    )
+} else {
+    throw new Error(
+        "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+    )
 }
 
-const container = createRoot(root)
-
-container.render(<App/>)
+console.log('Store state:', store.getState())
+console.log('Store:', store)
